1e5bc6c0e155cf0594b9a9ea79905e35d1b2050f
linbin <289873007@qq.com>
2020-01-29 20:08:15 +0800

    add cheerio to 分析页面

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,15 +1,39 @@
 // ts -> .d.ts 翻译文件 @types/xxx -> js
 
 import superagent from 'superagent'
+import cheerio from 'cheerio'
+
+interface Course {
+  title: string
+  count: number
+}
 
 class Crowller{
   private key = 'secretKey'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.key}`
-  private rawHtml = ''
+
+  getCourseInfo(html: string){
+    const $ = cheerio.load(html)
+    const courseItem = $('.course-item')
+    const courseInfos: Course[] = []
+    courseItem.map((index, element) => {
+      const desc = $(element).find('.course-desc')
+      const title = desc.eq(0).text()
+      const count = parseInt(desc.eq(1).text().split('：')[1])
+      courseInfos.push({
+        title, count
+      })
+    })
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    }
+    console.log(result)
+  }
 
   async getRawHtml(){
     const result = await superagent.get(this.url)
-    console.log(result.text)
+    this.getCourseInfo(result.text)
   }
 
   constructor(){
