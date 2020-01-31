247fb115cfd8b9d5c6f88d08f3e5ce1345adbdd4
linbin <289873007@qq.com>
2020-01-31 18:13:30 +0800

    add namespace

---
+++ src/namespace/demo.ts
@@ -0,0 +1,30 @@
+namespace Demo{
+  export class A{
+    constructor(){
+      const x = document.createElement('div')
+      x.innerHTML = 'A'
+      document.body.appendChild(x)
+    }
+  }
+  export class B{
+    constructor(){
+      const x = document.createElement('div')
+      x.innerHTML = 'B'
+      document.body.appendChild(x)
+    }
+  }
+  export class C{
+    constructor(){
+      const x = document.createElement('div')
+      x.innerHTML = 'C'
+      document.body.appendChild(x)
+    }
+  }
+  export class D{
+    constructor(){
+      const x = document.createElement('div')
+      x.innerHTML = 'D'
+      document.body.appendChild(x)
+    }
+  }
+}
---
+++ src/namespace/test.ts
@@ -0,0 +1,14 @@
+
+/// <reference path="./demo.ts" />
+// 只能拿来做提示，不能让打包的时候将两个文件打包在一起， 所以还得单独设置
+
+namespace Test{
+  export class Todo{
+    constructor(){
+      new Demo.A()
+      new Demo.B()
+      new Demo.C()
+      new Demo.D()
+    }
+  }
+}
