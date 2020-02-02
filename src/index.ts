33a178cf4dac9142fb37ba9f9a97a47d87eb5e38
linbin <289873007@qq.com>
2020-02-02 21:45:20 +0800

    add express login

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 import router from './router'
+import bodyParser from "body-parser";
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(router)
 
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,35 @@
-import {Router} from 'express'
+import {Router, Request} from 'express'
 import doSomething from './crowller'
 
 const router = Router()
 
-router.get('/aaa', (req, res) => {
-  doSomething()
-  res.end('done success')
+interface LoginRequest extends Request {
+  body: {
+    password ?: string
+  }
+}
+
+router.post('/aaa', (req: LoginRequest, res) => {
+  const { password } = req.body;
+  if(password === '123'){
+    console.log(password)
+    res.end('done success')
+  }else{
+    res.end(`error :: ${password}`)
+  }
 })
+
+router.get('/login', (req, res)=> {
+  res.end(`<html>
+    <body>
+      <form action="/aaa" method="post">
+        <input type="password" name="password" />
+        <button type="submit">tijiao</button>
+      </form>
+    </body>
+  </html>`)
+})
+
 router.get('/', (req, res) => {
   res.end('hello world')
 })
