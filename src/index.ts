4b6e498184700fbacc1cee3d7ff598ed2d9d8272
linbin <289873007@qq.com>
2020-02-03 20:20:11 +0800

    add islogin

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import express from 'express'
 import router from './router'
 import bodyParser from "body-parser";
-
+import cookieSession from "cookie-session";
 const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'text/html;charset=utf-8');//设置response编码为utf-8
+  next()
+})
+
+// session
+app.use(cookieSession({
+  name: 'session',
+  keys: ['billy'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
-import {Router, Request} from 'express'
+import {Router, Request, Response} from 'express'
 import doSomething from './crowller'
+import fs from 'fs'
+import path from 'path'
 
 const router = Router()
 
@@ -11,27 +13,99 @@ interface LoginRequest extends Request {
 
 router.post('/aaa', (req: LoginRequest, res) => {
   const { password } = req.body;
-  if(password === '123'){
-    console.log(password)
-    res.end('done success')
+  if(req.session){
+    const { isLogin } = req.session
+    if(!isLogin){
+      if(password === '123'){
+        req.session.isLogin = true
+        returnRes(res, '登录成功', 'search', '爬')
+        res.end('')
+      }else{
+        returnRes(res, '密码错误', 'login', '登陆')
+      }
+    }else{
+      returnRes(res, '已经登陆', 'search', '爬')
+    }
   }else{
-    res.end(`error :: ${password}`)
+    res.end('session 设置失败')
+  }
+})
+
+router.get('/search', (req, res) => {
+  if(req.session){
+    const { isLogin } = req.session
+    console.log('x::', isLogin)
+    if(isLogin){
+      doSomething()
+      returnRes(res, '爬取成功', 'seeFile', '查看文件内容')
+    }else{
+      returnRes(res, '未登录', 'login', '登陆')
+    }
+  }else{
+    res.end('session 设置失败')
+  }
+})
+
+router.get('/seeFile', (req, res) => {
+  const filePath = path.join(__dirname, '../data/course.json')
+  if(fs.existsSync(filePath)){
+    const text = fs.readFileSync(filePath, 'utf8')
+    res.json(JSON.parse(text))
+  }else{
+    returnRes(res, '文件不存在，请先爬取', 'search', '爬')
   }
 })
 
 router.get('/login', (req, res)=> {
-  res.end(`<html>
-    <body>
-      <form action="/aaa" method="post">
-        <input type="password" name="password" />
-        <button type="submit">tijiao</button>
-      </form>
-    </body>
-  </html>`)
+  if(req.session){
+    const { isLogin } = req.session
+    if(isLogin){
+      returnRes(res, '已经登陆', 'logout', '注销')
+    }else{
+      res.end(`<html>
+        <body>
+          <form action="/aaa" method="post">
+            <input type="password" name="password" />
+            <button type="submit">tijiao</button>
+          </form>
+        </body>
+      </html>`)
+    }
+  }else{
+    res.end('session 设置失败')
+  }
+})
+
+router.get('/logout', (req, res) => {
+  if(req.session){
+    const { isLogin } = req.session
+    let text: string
+    if(isLogin){
+      req.session.isLogin = false
+      text = '已经注销'
+      
+    }else{
+      text = '未登录'
+    }
+    returnRes(res, text, 'login', '登陆')
+  }else{
+    res.end('session 设置失败')
+  }
 })
 
 router.get('/', (req, res) => {
-  res.end('hello world')
+  returnRes(res, 'hello world', 'login', '请登陆')
 })
+
+function returnRes(res: Response, text: string, url: string, urltext:string){
+  res.end(`
+    <html>
+      <body>
+        <h1>${text}</h1>
+        <a href="/${url}">${urltext}</a>
+      </body>
+    </html>
+  `)
+}
 
 export default router
