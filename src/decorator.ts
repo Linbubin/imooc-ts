154483df1609cb173ed06cbc424a3c087da0e2a0
linbin <289873007@qq.com>
2020-02-05 18:13:17 +0800

    装饰器3 - set/get

--- src/decorator.ts
+++ src/decorator.ts
@@ -17,27 +17,51 @@
 //     this.name = name
 //   }
 // })
-
 // const test = new Tesst('wuqiu')
-
 // console.log(test.getName())
 
-// 方法装饰器
-function funcDecorator(obj: any, key: string, desc: PropertyDescriptor){
-  // 如果是static静态方法 { [Function: Todo] getName: [Function] } 'getName'
-  // 普通方法 Todo { getName: [Function] } 'getName'
-  // console.log(obj, key, desc)
 
-  desc.value = () => '1111'
+
+// 方法装饰器
+// function funcDecorator(obj: any, key: string, desc: PropertyDescriptor){
+//   // 如果是static静态方法 { [Function: Todo] getName: [Function] } 'getName'
+//   // 普通方法 Todo { getName: [Function] } 'getName'
+//   // console.log(obj, key, desc)
+
+//   desc.value = () => '1111'
+// }
+// class Todo{
+//   constructor(private name:string="billy"){}
+
+//   @funcDecorator
+//   getName(){
+//     return this.name
+//   }
+// }
+
+// const a = new Todo()
+// console.log(a.getName())
+
+
+// 访问器的装饰器
+function visitDecorator(obj: any, key: string, desc: PropertyDescriptor){
+  console.log(obj, key, desc)
+  // 也可以修改writable 来使其不能被改变
+  desc.writable = false
 }
 class Todo{
-  constructor(private name:string="billy"){}
-
-  @funcDecorator
-  getName(){
-    return this.name
+  private _name: string
+  constructor(name:string){
+    this._name = name
+  }
+  @visitDecorator
+  set name(v:string){
+    this._name = v
+  }
+  get name(){
+    return this._name
   }
 }
-
-const a = new Todo()
-console.log(a.getName())
+const a = new Todo('billy')
+a.name = '123'
+console.log(a.name)
