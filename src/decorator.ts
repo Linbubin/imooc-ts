9c0776abcf3b12dfaa3da676976e132fe466a7be
linbin <289873007@qq.com>
2020-02-05 16:59:28 +0800

    装饰器-2 func

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,22 +1,43 @@
 
-function Todo(){
-  return function <T extends new (...arg:any[])=>any>(constructor: T){
-    return class extends constructor{
-      name = 'billy'
-      getName(){
-        return this.name
-      }
-    }
+// 类装饰器
+// function Todo(){
+//   return function <T extends new (...arg:any[])=>any>(constructor: T){
+//     return class extends constructor{
+//       name = 'billy'
+//       getName(){
+//         return this.name
+//       }
+//     }
+//   }
+// }
+
+// const Tesst = Todo()(class Person{
+//   name: string
+//   constructor(name: string){
+//     this.name = name
+//   }
+// })
+
+// const test = new Tesst('wuqiu')
+
+// console.log(test.getName())
+
+// 方法装饰器
+function funcDecorator(obj: any, key: string, desc: PropertyDescriptor){
+  // 如果是static静态方法 { [Function: Todo] getName: [Function] } 'getName'
+  // 普通方法 Todo { getName: [Function] } 'getName'
+  // console.log(obj, key, desc)
+
+  desc.value = () => '1111'
+}
+class Todo{
+  constructor(private name:string="billy"){}
+
+  @funcDecorator
+  getName(){
+    return this.name
   }
 }
 
-const Tesst = Todo()(class Person{
-  name: string
-  constructor(name: string){
-    this.name = name
-  }
-})
-
-const test = new Tesst('wuqiu')
-
-console.log(test.getName())
+const a = new Todo()
+console.log(a.getName())
